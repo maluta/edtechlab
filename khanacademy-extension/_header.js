23cbc87c6af7e78cf10d8fdb709b39faf01a8429
Tiago Maluta <tiago.maluta@gmail.com>
2017-04-16 10:39:17 -0300

    chrome extension

---
+++ khanacademy-extension/_header.js
@@ -0,0 +1,4 @@
+/* remove header */
+var c1 = document.getElementById("outer-wrapper");
+var c2 = document.getElementById("top-header-container");
+var garbage = c1.removeChild(c2);
---
+++ khanacademy-extension/popup.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var btnLogo = document.getElementById('btnLogo');
+  var btnHeader = document.getElementById('btnHeader');
+  var btnRestore = document.getElementById('btnRestore');
+
+  btnLogo.addEventListener('click', function() {
+    chrome.tabs.executeScript(null, {file: "_logo.js"});
+  }, false);
+
+  btnHeader.addEventListener('click', function() {
+    chrome.tabs.executeScript(null, {file: "_header.js"});
+  }, false);
+
+  btnRestore.addEventListener('click', function() {
+      chrome.tabs.reload();
+    }, false);
+
+}, false); /* end: addEventListener */
